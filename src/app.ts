85a4081343f70f917add27ca596380a3d595ca36
crsz20 <cris14.alex@gmail.com>
2020-11-11 14:15:46 -0600

    Added basic recognition of users joining/leaving a voice channel

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import * as Discord from "discord.js";
 
 export const client = new Discord.Client();                                 //For interacting with the Discord API. This client represents the discord bot.
 import "./messages";
+import "./logging";
 
 //client.login(process.env.BOT_TOKEN);                                      //Login method for having the Discord API identify the bot via env variables, for Heroku
 console.log("Started bot!");
