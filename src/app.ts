f715f9c884d35537da9197ed507f9877f1e94d51
Vedansh Patel <vxp170009@utdallas.edu>
2020-11-11 22:58:11 -0600

    set default timezone

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 require("dotenv").config();
 import * as Discord from "discord.js";
+import { Settings, DateTime } from "luxon";
+Settings.defaultZoneName = "America/Chicago";
 
 export const client = new Discord.Client();                                 //For interacting with the Discord API. This client represents the discord bot.
 import "./messages";
 import "./watcher";
 import "./helpers/schedule";
 
-console.log("Started bot!");
+console.log(`Started bot at ${DateTime.local().toISOTime()}`);
 client.login(process.env.BOT_TOKEN);                                         //Login method for having the Discord API identify the bot via env variables, for Heroku
